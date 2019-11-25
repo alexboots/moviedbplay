24142f82eeaee184f41146eaedaf0fbdfc9ccf5d
Alex Hays <alexhays@gmail.com>
2019-11-25 15:46:23 -0500

    add default placeholder value to search

--- client/src/components/Home/SearchBar/index.js
+++ client/src/components/Home/SearchBar/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Search, Input, Clear, ClearWrapper } from './styles'
 
-export const SearchBar = ({setValue, value, placeholder}) => {
+export const SearchBar = ({setValue, value, placeholder='Search...'}) => {
   const handleChange = (event) => {
     setValue(event.target.value)
   }
 
   return (
     <Search>
-      <Input value={value} onChange={handleChange} placeholder="placeholder" />
+      <Input value={value} onChange={handleChange} placeholder={placeholder} />
       <ClearWrapper>
         <Clear onClick={()=>{setValue('')}}>&#10006;</Clear>
       </ClearWrapper>
