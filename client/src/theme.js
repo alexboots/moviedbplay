b761dc148de82a19c658195f2fc2f39d645523a6
Alex Hays <alexhays@gmail.com>
2019-11-25 15:06:14 -0500

    remove trailing , from theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,35 +1,35 @@
 // colours lifted from spectrum :P https://github.com/withspectrum/spectrum/blob/alpha/shared/theme/index.js
 export const theme = {
   bg: {
-    default: '#FFFFFF',
+    default: '#FFFFFF'
   },
   brand: {
     default: '#4400CC',
     wash: '#E8E5FF',
     dark: '#2A0080',
-    wash: '#E8E5FF',
+    wash: '#E8E5FF'
   },
   generic: {
-    alt: '#F6FBFC',
+    alt: '#F6FBFC'
   },
   special: {
-    wash: '#FFF5E5',
+    wash: '#FFF5E5'
   },
   space: {
-    alt: '#1CD2F2',
+    alt: '#1CD2F2'
   },
   text: {
     default: '#24292E',
     alt: '#67717A',
     secondary: '#384047',
-    reverse: '#FFFFFF',
+    reverse: '#FFFFFF'
   },
   success: {
-    default: '#00B88B',
+    default: '#00B88B'
   },
   warn: {
     default: '#E22F2F',
-    wash: '#FFEDF6',
+    wash: '#FFEDF6'
   }
 };
 
